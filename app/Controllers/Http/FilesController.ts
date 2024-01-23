b45d354fc1cd67522f192b9e537ac360cf16b440
Mortvola <mortvola@gmail.com>
2024-01-23 10:11:29 -0700

    Initial commit.

---
+++ app/Controllers/Http/FilesController.ts
@@ -0,0 +1,62 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Drive from '@ioc:Adonis/Core/Drive'
+import ShaderDescriptor from 'App/Models/ShaderDescriptor'
+
+export default class FilesController {
+  public async getShaderDescriptor ({ params }: HttpContextContract): Promise<unknown> {
+    const descriptor = await ShaderDescriptor.findOrFail(params.id)
+
+    return {
+      name: descriptor.name,
+      descriptor: descriptor.descriptor,
+    }
+  }
+
+  public async uploadShaderDescriptor ({ request }: HttpContextContract): Promise<{ id: number }> {
+    try {
+      const t = request.body()
+
+      const shaderDescriptor = await ShaderDescriptor.create({
+        name: t.name,
+        descriptor: t.descriptor,
+      })
+
+      return ({ id: shaderDescriptor.id })
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+  }
+
+  public async updateShaderDescriptor ({ request, params }: HttpContextContract): Promise<void> {
+    try {
+      const shaderDescriptor = await ShaderDescriptor.findOrFail(params.id)
+
+      const t = request.body()
+
+      shaderDescriptor.merge({
+        name: t.name,
+        descriptor: t.descriptor,
+      })
+
+      await shaderDescriptor.save()
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+  }
+
+  public async uploadMaterial ({ request, params }: HttpContextContract): Promise<void> {
+    try {
+      await Drive.put(`material/${params.file}`, JSON.stringify(request.body()))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  public async getShaderList ({}: HttpContextContract): Promise<{ id: number, name: string }[]> {
+    const descriptors = await ShaderDescriptor.all()
+
+    return descriptors.map((d) => ({ id: d.id, name: d.name }))
+  }
+}
---
+++ start/routes.ts
@@ -0,0 +1,33 @@
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| This file is dedicated for defining HTTP routes. A single file is enough
+| for majority of projects, however you can define routes in different
+| files and just make sure to import them inside this file. For example
+|
+| Define routes in following two files
+| ├── start/routes/cart.ts
+| ├── start/routes/customer.ts
+|
+| and then import them inside `start/routes.ts` as follows
+|
+| import './routes/cart'
+| import './routes/customer''
+|
+*/
+
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.get('/', async ({ view }) => {
+  return view.render('welcome')
+})
+
+Route.post('/shader-descriptors', 'FilesController.uploadShaderDescriptor')
+Route.patch('/shader-descriptors/:id', 'FilesController.updateShaderDescriptor')
+Route.get('/shader-descriptors/:id', 'FilesController.getShaderDescriptor')
+
+Route.post('/material/:file', 'FilesController.uploadMaterial')
+
+Route.get('/shader-list', 'FilesController.getShaderList')
