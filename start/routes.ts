0beb97e5ca953100e08d1ec99e612f39571d8140
Mortvola <mortvola@gmail.com>
2024-09-04 05:19:04 -0600

    Added a route grouping for /api.

--- start/routes.ts
+++ start/routes.ts
@@ -27,78 +27,79 @@ Route.get('/', async ({ view }) => {
 })
 
 Route.group(() => {
-  Route.post('', 'FilesController.uploadShaderDescriptor')
-  Route.patch('/:id', 'FilesController.updateShaderDescriptor')
-  Route.delete('/:id', 'FilesController.deleteShaderDescriptor')
-  Route.get('/:id', 'FilesController.getShaderDescriptor')
-}).prefix('/api/shader-descriptors')
+  Route.group(() => {
+    Route.get('/:id', 'ProjectsController.get')
+  }).prefix('/projects')
 
-Route.get('/api/shader-list', 'FilesController.getShaderList')
+  Route.group(() => {
+    Route.post('', 'FilesController.uploadShaderDescriptor')
+    Route.patch('/:id', 'FilesController.updateShaderDescriptor')
+    Route.delete('/:id', 'FilesController.deleteShaderDescriptor')
+    Route.get('/:id', 'FilesController.getShaderDescriptor')
+  }).prefix('/shader-descriptors')
 
-Route.group(() => {
-  Route.post('', 'MaterialsController.uploadMaterial')
-  Route.get('/:id', 'MaterialsController.getMaterial')
-  Route.patch('/:id', 'MaterialsController.updateMaterial')
-  Route.delete('/:id', 'MaterialsController.deleteMaterial')
-}).prefix('/api/materials')
+  Route.get('/shader-list', 'FilesController.getShaderList')
 
-Route.get('/api/materials-list', 'MaterialsController.getMaterialList')
+  Route.group(() => {
+    Route.post('', 'MaterialsController.uploadMaterial')
+    Route.get('/:id', 'MaterialsController.getMaterial')
+    Route.patch('/:id', 'MaterialsController.updateMaterial')
+    Route.delete('/:id', 'MaterialsController.deleteMaterial')
+  }).prefix('/materials')
 
-Route.group(() => {
-  Route.post('', 'ModelsController.uploadModel')
-  Route.get('/:id', 'ModelsController.getModel')
-  Route.patch('/:id', 'ModelsController.updateModel')
-  Route.delete('/:id', 'ModelsController.deleteModel')
-}).prefix('/api/models')
+  Route.get('/materials-list', 'MaterialsController.getMaterialList')
 
-Route.get('/api/models-list', 'ModelsController.getModelList')
+  Route.group(() => {
+    Route.post('', 'ModelsController.uploadModel')
+    Route.get('/:id', 'ModelsController.getModel')
+    Route.patch('/:id', 'ModelsController.updateModel')
+    Route.delete('/:id', 'ModelsController.deleteModel')
+  }).prefix('/models')
 
-Route.group(() => {
-  Route.post('', 'TexturesController.uploadTexture')
-  Route.get('/:id', 'TexturesController.getTexture')
-  Route.patch('/:id', 'TexturesController.updateTexture')
-  Route.get('/:id/file', 'TexturesController.getTextureFile')
-  Route.delete('/:id', 'TexturesController.deleteTexture')
-}).prefix('/api/textures')
+  Route.get('/models-list', 'ModelsController.getModelList')
 
-Route.get('/api/textures-list', 'TexturesController.getTextureList')
+  Route.group(() => {
+    Route.post('', 'TexturesController.uploadTexture')
+    Route.get('/:id', 'TexturesController.getTexture')
+    Route.patch('/:id', 'TexturesController.updateTexture')
+    Route.get('/:id/file', 'TexturesController.getTextureFile')
+    Route.delete('/:id', 'TexturesController.deleteTexture')
+  }).prefix('/textures')
 
-Route.group(() => {
-  Route.post('', 'GameObjectsController.uploadGameObject')
-  Route.get('/:id', 'GameObjectsController.getGameObject')
-  Route.delete('/:id', 'GameObjectsController.deleteGameObject')
-  Route.patch('/:id', 'GameObjectsController.updateGameObject')
-}).prefix('/api/scene-objects')
+  Route.get('/textures-list', 'TexturesController.getTextureList')
 
-Route.get('/api/scene-objects-list', 'GameObjectsController.getGameObjectList')
+  Route.group(() => {
+    Route.post('', 'GameObjectsController.uploadGameObject')
+    Route.get('/:id', 'GameObjectsController.getGameObject')
+    Route.delete('/:id', 'GameObjectsController.deleteGameObject')
+    Route.patch('/:id', 'GameObjectsController.updateGameObject')
+  }).prefix('/scene-objects')
 
-Route.group(() => {
-  Route.post('', 'PrefabsController.post')
-  Route.get('/:id', 'PrefabsController.get')
-  Route.delete('/:id', 'PrefabsController.delete')
-  Route.patch('/:id', 'PrefabsController.patch')
-}).prefix('/api/prefabs')
+  Route.get('/scene-objects-list', 'GameObjectsController.getGameObjectList')
 
-Route.group(() => {
-  Route.get('', 'FoldersController.getFolder')
-  Route.get('/:id', 'FoldersController.getFolder')
-  Route.post('', 'FoldersController.makeFolder')
-  Route.post('/item', 'FoldersController.makeItem')
-  Route.patch('/:id', 'FoldersController.updateFolder')
-  Route.delete('/:id', 'FoldersController.deleteItem')
-}).prefix('/api/folders')
+  Route.group(() => {
+    Route.post('', 'PrefabsController.post')
+    Route.get('/:id', 'PrefabsController.get')
+    Route.delete('/:id', 'PrefabsController.delete')
+    Route.patch('/:id', 'PrefabsController.patch')
+  }).prefix('/prefabs')
 
-// Route.group(() => {
-//   Route.post('', 'ParticlesController.uploadParticle')
-//   Route.get('/:id', 'ParticlesController.getParticle')
-//   Route.patch('/:id', 'ParticlesController.updateParticle')
-// }).prefix('/api/particles')
+  Route.group(() => {
+    Route.get('', 'FoldersController.getFolder')
+    Route.get('/:id', 'FoldersController.getFolder')
+    Route.post('', 'FoldersController.makeFolder')
+    Route.post('/item', 'FoldersController.makeItem')
+    Route.patch('/:id', 'FoldersController.updateFolder')
+    Route.delete('/:id', 'FoldersController.deleteItem')
+  }).prefix('/folders')
 
-Route.group(() => {
-  Route.post('', 'ScenesController.uploadScene')
-  Route.get('/:id', 'ScenesController.getScene')
-  Route.patch('/:id', 'ScenesController.updateScene')
-}).prefix('/api/scenes')
+  Route.group(() => {
+    Route.post('', 'ScenesController.uploadScene')
+    Route.get('/:id', 'ScenesController.getScene')
+    Route.patch('/:id', 'ScenesController.updateScene')
+  }).prefix('/scenes')
+})
+  .prefix('/api')
 
 Route.get('/fonts/OpenSans-Regular-msdf.json', async ({ response }: HttpContextContract) => {
   response.stream(await Drive.getStream('/fonts/OpenSans-Regular-msdf.json'))
