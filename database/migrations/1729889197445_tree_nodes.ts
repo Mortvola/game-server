b86683cbcfe83e0b7cb1489ac219e77a5eec18b2
Mortvola <mortvola@gmail.com>
2024-10-25 14:59:57 -0600

    Drop unused column in tree_nodes

---
+++ database/migrations/1729889197445_tree_nodes.ts
@@ -0,0 +1,17 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'tree_nodes'
+
+  public async up () {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.dropColumn('tree_id')
+    })
+  }
+
+  public async down () {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.integer('tree_id')
+    })
+  }
+}
